3b7eb6e3450c94c7e26979e5774ed1e59c60edda
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-06-01 18:02:49 -0500

    Entendiendo los portals de React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 
-const Computacion = () => (
-  <React.Fragment>
-    <li>Monitor</li>
-    <li>Mouse</li>
-    <li>Teclado</li>
-  </React.Fragment>
-)
-const Ropa = () => (
-  <>
-    <li>Playera</li>
-    <li>Jeans</li>
-    <li>Shors</li>
-  </>
-)
-
+class PortalModal extends Component {
+  render() {
+    if (!this.props.visible) {
+      return null
+    }
+    const styles = {
+      width: '100%',
+      height: '100%',
+      position: 'absolute',
+      top: '0',
+      left: '0',
+      background: 'Linear-gradient(to top right, #667eea, #764ba2)',
+      opacity: '0.95',
+      color: '#FFF',
+    }
+    return ReactDOM.createPortal(
+      <div style={styles}>{this.props.children}</div>,
+      document.getElementById('modal-root')
+    )
+  }
+}
 class App extends Component {
+  state = {
+    visible: false,
+    num: 0,
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState((state) => ({
+        num: state.num + 1,
+      }))
+    }, 1000)
+  }
+  mostrar = () => {
+    this.setState({
+      visible: true,
+    })
+  }
+  cerrar = () => {
+    this.setState({
+      visible: false,
+    })
+  }
   render() {
     return (
       <div>
-        <Computacion />
-        <Ropa />
+        <button onClick={this.mostrar}>Mostrar</button>
+        <PortalModal visible={this.state.visible}>
+          <button onClick={this.cerrar}>Cerrar</button>
+          <h1>Hola desde un Protal Modal {this.state.num}</h1>
+        </PortalModal>
       </div>
     )
   }
