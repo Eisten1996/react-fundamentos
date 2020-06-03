c69b6c7bf4e1009f23a625293953447cb85e2b79
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-06-02 22:29:15 -0500

    Pasar datos al iterar listas con React

--- src/App.js
+++ src/App.js
@@ -1,49 +1,43 @@
 import React, { Component } from 'react'
-import styles from './App.module.css'
-
-const images = [
-  {
-    author: {
-      name: 'Vlad Bagacian',
-      avatar:
-        'https://images.pexels.com/users/avatars/381880/vlad-bagacian-302.jpeg?w=256&h=256&fit=crop&crop=faces',
-    },
-    source:
-      'https://images.pexels.com/photos/1368388/pexels-photo-1368388.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=400',
-    views: 153,
-    id: 1,
-  },
-  {
-    author: {
-      name: 'Moreno Matković',
-      avatar:
-        'https://images.pexels.com/users/avatars/1265479/moreno-matkovic-149.jpeg?w=256&h=256&fit=crop&crop=faces',
-    },
-    source:
-      'https://images.pexels.com/photos/2779539/pexels-photo-2779539.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=400',
-    views: 214,
-    id: 2,
-  },
-]
-
-const Image = ({ image }) => (
-  <div className={styles.card}>
-    <img src={image.source} alt="imagen" className={styles.image} />
-    <div className={styles.footer}>
-      <img src={image.author.avatar} alt="autor" className={styles.avatar} />
-      <div>{image.author.name}</div>
-      <div>{image.views}</div>
-    </div>
-  </div>
-)
 
 class App extends Component {
+  state = {
+    fruits: [
+      { name: 'Fresa', price: 5.4 },
+      { name: 'Manzana', price: 2.3 },
+      { name: 'Sandia', price: 1.2 },
+      { name: 'Guayaba', price: 5 },
+      { name: 'Pera', price: 4.12 },
+      { name: 'Kiwi', price: 3.87 },
+      { name: 'Limon', price: 7.21 },
+      { name: 'Melon', price: 6.21 },
+      { name: 'Cereza', price: 4.98 },
+    ],
+    frutaSeleccionada: {},
+  }
+
+  select = (frutaSeleccionada) => {
+    console.log(frutaSeleccionada)
+    this.setState({ frutaSeleccionada })
+  }
+
   render() {
+    const { fruits, frutaSeleccionada } = this.state
     return (
-      <div className={styles.images}>
-        {images.map((image) => (
-          <Image image={image} key={image.id} />
-        ))}
+      <div>
+        <ul>
+          {fruits.map((fruit) => (
+            <li
+              key={fruit.name}
+              onClick={this.select.bind('this', fruit)}
+              style={{
+                color: frutaSeleccionada.name === fruit.name ? 'red' : '#000',
+              }}
+            >
+              {fruit.name} - $ {fruit.price}
+            </li>
+          ))}
+        </ul>
       </div>
     )
   }
