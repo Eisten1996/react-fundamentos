5200515aeb9b0019c8bfc4b2fa79def8da8ec1b0
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-06-03 17:10:35 -0500

    Usando el atributo for en formularios

--- src/App.js
+++ src/App.js
@@ -1,74 +1,47 @@
 import React, { Component } from 'react'
 
-class InputControlado extends Component {
-  state = {
-    text: '',
-    // tieneError: false,
-    color: '#E8E8E8',
+const Unicorn = () => (
+  <span role="img" aria-label="unicornio">
+    🦄
+  </span>
+)
+
+class InputNoControlado extends Component {
+  handleSubmit = (event) => {
+    event.preventDefault()
+    const nombre = event.target[0].value
+    const email = event.target[1].value
+
+    // manejo de datos
+    this.props.onSend({ nombre, email })
   }
-
-  actualizar = (event) => {
-    const text = event.target.value
-    // const tieneError = text !== '' && text.length < 5
-    let color = 'green'
-    if (text.trim() === '') {
-      color = '#E8E8E8'
-    }
-    if (text.trim() !== '' && text.trim().length < 5) {
-      color = 'red'
-    }
-    this.setState({
-      text,
-      color,
-    })
-
-    this.props.onChange(this.props.name, text)
-  }
-
   render() {
-    const styles = {
-      border: `1px solid ${this.state.color}`,
-      padding: '0.3em 0.6em',
-      outline: 'none',
-    }
     return (
-      <input
-        type="text"
-        value={this.state.text}
-        onChange={this.actualizar}
-        placeholder={this.props.placeholder}
-        style={styles}
-      />
+      <form onSubmit={this.handleSubmit}>
+        <p>
+          <label htmlFor="name">Nombre : </label>
+          <input id="name" type="text" placeholder="Nombres" />
+        </p>
+        <p>
+          <label htmlFor="email">Email : </label>
+          <input id="email" type="text" placeholder="Email" />
+        </p>
+        <button>Enviar</button>
+      </form>
     )
   }
 }
-
 class App extends Component {
-  state = {
-    name: '',
-    email: '',
-  }
-  actualizar = (name, text) => {
-    this.setState({
-      [name]: text,
-    })
+  send = (data) => {
+    console.log(data)
   }
   render() {
     return (
       <div>
-        <h1>Inputs Contralados</h1>
-        <InputControlado
-          onChange={this.actualizar}
-          placeholder="Nombre Completo"
-          name="name"
-        />
-        <InputControlado
-          onChange={this.actualizar}
-          placeholder="Email"
-          name="email"
-        />
-        <h2>NOMBRE : {this.state.name}</h2>
-        <h2>EMAIL : {this.state.email}</h2>
+        <h1>
+          Atributos con Refs <Unicorn />
+        </h1>
+        <InputNoControlado onSend={this.send} />
       </div>
     )
   }
