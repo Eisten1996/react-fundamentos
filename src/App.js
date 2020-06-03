43d22a8b4ecec8b9a771535f2eac79776dcab7ab
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-06-02 22:55:18 -0500

    Que son las refs en React

--- src/App.js
+++ src/App.js
@@ -1,43 +1,33 @@
 import React, { Component } from 'react'
 
-class App extends Component {
-  state = {
-    fruits: [
-      { name: 'Fresa', price: 5.4 },
-      { name: 'Manzana', price: 2.3 },
-      { name: 'Sandia', price: 1.2 },
-      { name: 'Guayaba', price: 5 },
-      { name: 'Pera', price: 4.12 },
-      { name: 'Kiwi', price: 3.87 },
-      { name: 'Limon', price: 7.21 },
-      { name: 'Melon', price: 6.21 },
-      { name: 'Cereza', price: 4.98 },
-    ],
-    frutaSeleccionada: {},
+class Entrada extends Component {
+  entrada = React.createRef()
+  componentDidMount() {
+    this.focus()
   }
 
-  select = (frutaSeleccionada) => {
-    console.log(frutaSeleccionada)
-    this.setState({ frutaSeleccionada })
+  focus = () => {
+    this.entrada.current.focus()
   }
-
+  blur = () => {
+    this.entrada.current.blur()
+  }
   render() {
-    const { fruits, frutaSeleccionada } = this.state
     return (
       <div>
-        <ul>
-          {fruits.map((fruit) => (
-            <li
-              key={fruit.name}
-              onClick={this.select.bind('this', fruit)}
-              style={{
-                color: frutaSeleccionada.name === fruit.name ? 'red' : '#000',
-              }}
-            >
-              {fruit.name} - $ {fruit.price}
-            </li>
-          ))}
-        </ul>
+        <input type="text" ref={this.entrada} />
+        <button onClick={this.focus}>Focus</button>
+        <button onClick={this.blur}>Blur</button>
+      </div>
+    )
+  }
+}
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>React refs</h1>
+        <Entrada />
       </div>
     )
   }
