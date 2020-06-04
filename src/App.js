3d17a491566185cc048804fa8e2131ebc1c7a58d
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-06-04 17:29:31 -0500

    Solución a Tarea (buscador de películas)

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import React, { Component } from 'react'
 class App extends Component {
   state = {
     movie: {},
+    isFeaching: false,
   }
 
   handlerSubmit = (event) => {
     event.preventDefault()
+    this.setState({ isFeaching: true })
     const title = event.target[0].value
-    alert(title)
+    // alert(title)
     const url = 'http://www.omdbapi.com/?apikey=76ad082'
     fetch(url + '&t=' + title)
       .then((res) => res.json())
-      .then((movie) => this.setState({ movie }))
+      .then((movie) => this.setState({ movie, isFeaching: false }))
   }
 
   render() {
-    const { movie } = this.state
+    const { movie, isFeaching } = this.state
     return (
       <div>
         <h1>Ejemplo HTTP Buscador de Peliculas</h1>
@@ -24,11 +26,14 @@ class App extends Component {
           <input type="text" placeholder="Nombre de la pelicula"></input>
           <button>Buscar</button>
         </form>
-        <div>
-          <h1>{movie.Title}</h1>
-          <p>{movie.Plot}</p>
-          <img src={movie.Poster} alt="poster"></img>
-        </div>
+        {isFeaching && <h2>Cargando .... </h2>}
+        {movie.Title && !isFeaching && (
+          <div>
+            <h1>{movie.Title}</h1>
+            <p>{movie.Plot}</p>
+            <img src={movie.Poster} alt="poster"></img>
+          </div>
+        )}
       </div>
     )
   }
