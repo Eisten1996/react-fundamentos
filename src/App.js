57cab99957f37c461e939bbd3711c4b3fac208d9
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-06-12 19:20:33 -0500

    Comunicación Event Bubbling (Hijo a Padre)
    es un antipatron

--- src/App.js
+++ src/App.js
@@ -5,20 +5,11 @@ const Header = () => {
     fontWeigth: 'blod',
   }
 
-  const headerStyle = {
-    margin: '0.6em',
-    borderRadius: '0.3em',
-    border: '1px solid #d2d2d2',
-    padding: '2em 0.4em',
-    fontFamily: 'monoespace',
-    fontSize: '17px',
-  }
-
   return (
     <header style={headerStyle}>
-      <div>Comunicacion entre componente</div>
+      <div>(Hijo a Padre)</div>
       <div style={subtitleSyle}>
-        Metodos de Instancia
+        Event Bubbling
         <span role="img" aria="img">
           🔥
         </span>
@@ -27,38 +18,39 @@ const Header = () => {
   )
 }
 
-class Hijo extends Component {
-  state = {
-    mensaje: '****',
-  }
+const headerStyle = {
+  margin: '0.5em',
+  padding: '0.5em',
+  borderRadius: '0.3em',
+  border: '1px solid gray',
+  textAlign: 'center',
+}
 
-  dispatchAlert = (e, mensaje = 'Alert desde el hijo') => {
-    alert(mensaje)
-    this.setState({ mensaje })
+class Hijo extends Component {
+  handlerClick = (e) => {
+    // e.stopPropagation()
+    e.saludo = 'Hola mensaje desde el hijo'
+    console.log('Click en Hijo')
   }
 
   render() {
     return (
-      <div>
-        <h2>{this.state.mensaje}</h2>
-        <button onClick={this.dispatchAlert}>Hijo</button>
+      <div style={headerStyle} onClick={this.handlerClick}>
+        <p>Hijo</p>
       </div>
     )
   }
 }
-
 class App extends Component {
-  hijo = React.createRef()
-
-  handlerClick = () => {
-    this.hijo.current.dispatchAlert(null, 'Hola desde el padre')
+  handlerClick = (e) => {
+    console.log('Click en Padre', e.saludo)
   }
+
   render() {
     return (
-      <div>
+      <div style={headerStyle} onClick={this.handlerClick}>
         <Header />
-        <Hijo ref={this.hijo} />
-        <button onClick={this.handlerClick}>Padre</button>
+        <Hijo />
       </div>
     )
   }
